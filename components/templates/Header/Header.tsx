67f0982cd71971fe3ffc76b0e671124380fba684
Father1993 <enjoyhillol@gmail.com>
2024-11-20 12:03:39 +1000

    upda te and fix bugs

--- components/templates/Header/Header.tsx
+++ components/templates/Header/Header.tsx
@@ -1,17 +1,7 @@
-import Image from 'next/image'
-import logo from '@/public/img/original.png'
-
 const Header = () => {
     return (
         <header className="flex justify-center">
-            <Image
-                className="logo"
-                src={logo}
-                alt="Asia Motors logo"
-                width={200}
-                height={200}
-                priority
-            />
+            <img className="logo" src="/img/original.png" />
         </header>
     )
 }
