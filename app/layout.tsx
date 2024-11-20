ec7f9452479eee8be17389a59ebb676d47427d43
Father1993 <enjoyhillol@gmail.com>
2024-11-20 11:01:34 +1000

    chnage style and adaptive with mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,23 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-    title: 'Asia Motor Калькулятор',
-    description: 'Asia Motor Калькулятор, рассчитай свой авто сам',
+    title: 'Asia Motors Калькулятор',
+    description: 'Asia Motors Калькулятор, рассчитай свой авто сам',
+    manifest: '/site.webmanifest',
+    icons: {
+        apple: [{ url: '/apple-touch-icon.png' }],
+        icon: [
+            { url: '/favicon-32x32.png', sizes: '32x32' },
+            { url: '/favicon-16x16.png', sizes: '16x16' },
+        ],
+        other: [
+            {
+                rel: 'mask-icon',
+                url: '/safari-pinned-tab.svg',
+                color: '#5bbad5',
+            },
+        ],
+    },
 }
 
 export default function RootLayout({
